29c6c7eca7a3279f5d3cc29c22bf2cb55a34371d
Hemanth Kumar Rao M <mahendrakarhemanth@gmail.com>
2026-07-10 15:48:07 +0530

    Update mlflow_manager.py

--- mlflow_manager.py
+++ mlflow_manager.py
@@ -3,7 +3,11 @@ MLflow Manager
 """
 
 import mlflow
+import mlflow
+from config import *
 
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment(EXPERIMENT_NAME)
 def setup_mlflow():
     # Use SQLite backend
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
